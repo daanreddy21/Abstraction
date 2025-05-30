4eaa2ade14a8163657fee623feeecd6a1ea5c7f0
daanr <daanr@DAAN-LAP-IN-USE>
2025-05-30 15:04:30 +0530

    daan

---
+++ Abstraction1/src/Anonymous.java
@@ -0,0 +1,38 @@
+
+
+
+abstract class Plane {
+    void fly() {
+        System.out.println("Plane is ready to fly");
+    }
+
+    abstract int num();
+    abstract void name();
+
+    void go() {
+        System.out.println("Signal to fly");
+    }
+}
+
+public class Anonymous {
+    public static void main(String[] args) {
+        Plane c = new Plane() {
+            int passengers =56;
+
+            int num() {
+                return passengers;
+            }
+
+            void name() {
+                System.out.println("IndiGo");
+            }
+
+			
+        };
+
+        c.name();
+        System.out.println("Number of passengers: " + c.num());
+        c.fly();
+        c.go();
+    }
+}
